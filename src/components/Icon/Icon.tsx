31c5546f02f581affc96bd8606d1793ea1d6ef19
alvaro aguirre <alvaro1337@hotmail.com>
2021-08-09 01:34:14 -0300

    Add icon

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/display-name */
 import React, { FunctionComponent } from "react";
 import styled from "styled-components";
 
@@ -31,7 +32,7 @@ export const Icon: FunctionComponent<IconProps> = ({
     marginRight={marginRight}
     marginLeft={marginLeft}
   >
-    {getPath(icon, color)}
+    {paths[icon](color)}
   </SVG>
 );
 
@@ -40,25 +41,21 @@ const viewBox: Record<IconType, string> = {
   close: "0 0 320 512",
 };
 
-const getPath = (name: IconType, color: string) => {
-  switch (name) {
-    case "burger":
-      return (
-        <path
-          xmlns="http://www.w3.org/2000/svg"
-          d="M436 124H12c-6.627 0-12-5.373-12-12V80c0-6.627 5.373-12 12-12h424c6.627 0 12 5.373 12 12v32c0 6.627-5.373 12-12 12zm0 160H12c-6.627 0-12-5.373-12-12v-32c0-6.627 5.373-12 12-12h424c6.627 0 12 5.373 12 12v32c0 6.627-5.373 12-12 12zm0 160H12c-6.627 0-12-5.373-12-12v-32c0-6.627 5.373-12 12-12h424c6.627 0 12 5.373 12 12v32c0 6.627-5.373 12-12 12z"
-          fill={color}
-        />
-      );
-    case "close":
-      return (
-        <path
-          xmlns="http://www.w3.org/2000/svg"
-          d="M193.94 256L296.5 153.44l21.15-21.15c3.12-3.12 3.12-8.19 0-11.31l-22.63-22.63c-3.12-3.12-8.19-3.12-11.31 0L160 222.06 36.29 98.34c-3.12-3.12-8.19-3.12-11.31 0L2.34 120.97c-3.12 3.12-3.12 8.19 0 11.31L126.06 256 2.34 379.71c-3.12 3.12-3.12 8.19 0 11.31l22.63 22.63c3.12 3.12 8.19 3.12 11.31 0L160 289.94 262.56 392.5l21.15 21.15c3.12 3.12 8.19 3.12 11.31 0l22.63-22.63c3.12-3.12 3.12-8.19 0-11.31L193.94 256z"
-          fill={color}
-        />
-      );
-  }
+const paths: Record<IconType, (color: string) => JSX.Element> = {
+  burger: (color) => (
+    <path
+      xmlns="http://www.w3.org/2000/svg"
+      d="M436 124H12c-6.627 0-12-5.373-12-12V80c0-6.627 5.373-12 12-12h424c6.627 0 12 5.373 12 12v32c0 6.627-5.373 12-12 12zm0 160H12c-6.627 0-12-5.373-12-12v-32c0-6.627 5.373-12 12-12h424c6.627 0 12 5.373 12 12v32c0 6.627-5.373 12-12 12zm0 160H12c-6.627 0-12-5.373-12-12v-32c0-6.627 5.373-12 12-12h424c6.627 0 12 5.373 12 12v32c0 6.627-5.373 12-12 12z"
+      fill={color}
+    />
+  ),
+  close: (color) => (
+    <path
+      xmlns="http://www.w3.org/2000/svg"
+      d="M193.94 256L296.5 153.44l21.15-21.15c3.12-3.12 3.12-8.19 0-11.31l-22.63-22.63c-3.12-3.12-8.19-3.12-11.31 0L160 222.06 36.29 98.34c-3.12-3.12-8.19-3.12-11.31 0L2.34 120.97c-3.12 3.12-3.12 8.19 0 11.31L126.06 256 2.34 379.71c-3.12 3.12-3.12 8.19 0 11.31l22.63 22.63c3.12 3.12 8.19 3.12 11.31 0L160 289.94 262.56 392.5l21.15 21.15c3.12 3.12 8.19 3.12 11.31 0l22.63-22.63c3.12-3.12 3.12-8.19 0-11.31L193.94 256z"
+      fill={color}
+    />
+  ),
 };
 
 const SVG = styled.svg<{ marginRight?: string; marginLeft?: string }>`
