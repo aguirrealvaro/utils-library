f11444e9d4bf8c41245373ee9e44e6d5fe737da5
Alvaro Aguirre <alvaro1337@hotmail.com>
2021-08-16 18:33:38 -0300

    Remove TC

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,12 @@
 import React, { FunctionComponent } from "react";
 import { ThemeProvider } from "styled-components";
 import { theme, GlobalStyles } from ".";
-import { TC } from "../TC/TC";
 import { ToastProvider } from "@/components/Toast";
 
 export const App: FunctionComponent = () => {
   return (
     <ThemeProvider theme={theme}>
       <ToastProvider>
-        <TC />
         <GlobalStyles />
       </ToastProvider>
     </ThemeProvider>
