9273e67d87e5db6fd9e6e828fd8ee4020f62e797
Alvaro Aguirre <alvaro1337@hotmail.com>
2021-08-30 16:50:40 -0300

    Change css modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -61,8 +61,8 @@ const Backdrop = styled.div<{ show: boolean; fadeOut: boolean }>`
   font-family: Arial;
   top: 0;
   left: 0;
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
   overflow-y: auto;
   z-index: 1;
   animation: ${fadeIn} ${ANIMATION_TIME}ms ease-out;
