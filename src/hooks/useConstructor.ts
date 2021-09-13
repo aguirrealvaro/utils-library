c84398b425c58ffd7365f5df02260c95b10aece1
Alvaro Aguirre <alvaro1337@hotmail.com>
2021-09-13 02:08:49 -0300

    Delete useConstructor

--- src/hooks/useConstructor.ts
+++
@@ -1,8 +0,0 @@
-import { useRef } from "react";
-
-export const useConstructor = (callback: () => void): void => {
-  const ref = useRef<boolean>(false);
-  if (ref.current) return;
-  callback();
-  ref.current = false;
-};
